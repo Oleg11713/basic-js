d58c16b512ae1983251ea4ad8197efaa72d11105
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-11 20:42:45 +0300

    fix: fix bugs

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,7 +4,7 @@ module.exports = function createDreamTeam(members) {
   let team = '';
     for (let i = 0; i < members.length; i++){
       if(members[i].toString()[0] == members[i].toString()[0].toUpperCase())
-      team.push(members[i].toString()[0]);
+      team += members[i].toString()[0];
     }
     return team;
 };
