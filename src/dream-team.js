41d5c100df24bb7ec007036777570e2a06553d3e
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-11 20:52:45 +0300

    fix:

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
   let team = '';
     for (let i = 0; i < members.length; i++){
-      if(members[i].toString()[0] == members[i].toString()[0].toUpperCase())
+      if(members[i].split("")[0] == members[i].slpit("")[0].toUpperCase())
       team += members[i].toString()[0];
     }
     team.split("").sort().join("");
