1f785afdfefbb07ff945af905ccdc1bf663feff8
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-12 18:06:56 +0300

    fix: fix code

--- src/what-season.js
+++ src/what-season.js
@@ -1,25 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if(!data) {
+ if(!data) {
     return 'Unable to determine the time of year!';
   }
   if( isNaN(date.getTime())){  
     throw new Error();
   }
   let month = date.getMonth();
-  let season = "";
-  if (month >= 0 && month <= 1 || month == 11){
-    season = "winter";
+  if ((month >= 0 && month <= 1) || month == 11){
+    return 'winter';
   }
   if (month >= 2 && month <= 4){
-    season = "spring";
+    return 'spring';
   }
   if (month >= 5 && month <= 7){
-    season = "summer";
+    return 'summer';
   }
   if (month >= 8 && month <= 10){
-    season = "autumn";
+    return 'autumn';
   }
-  return season;
 };
