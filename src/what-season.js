4b69b7602ae1e53eb9b7341539e80eeabeeb807a
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-12 18:19:50 +0300

    fix: fix

--- src/what-season.js
+++ src/what-season.js
@@ -11,13 +11,14 @@ if (!data || data === undefined) {
   if ((month >= 0 && month <= 1) || month == 11){
     return 'winter';
   }
-  if (month >= 2 && month <= 4){
+  else if (month >= 2 && month <= 4){
     return 'spring';
   }
-  if (month >= 5 && month <= 7){
+  else if (month >= 5 && month <= 7){
     return 'summer';
   }
-  if (month >= 8 && month <= 10){
+  else if (month >= 8 && month <= 10){
     return 'autumn';
   }
+  else throw 'Not implemented';
 };
